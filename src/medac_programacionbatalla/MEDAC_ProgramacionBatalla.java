49f53dcc319e4d3905ef42baad8f2b5eeda0c090
darwinas05 <darwinalemansoza.2005@gmail.com>
2024-04-23 09:14:12 +0200

    primer commit conectando a la base de datos

---
+++ src/medac_programacionbatalla/MEDAC_ProgramacionBatalla.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package medac_programacionbatalla;
+
+import batallas.Batalla;
+
+/**
+ * @author danie
+ */
+public class MEDAC_ProgramacionBatalla {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+//        String test = ExploradorFicheros.leerFichero();
+        Batalla batalla = new Batalla();
+
+    }
+}
+    
+
+ 
